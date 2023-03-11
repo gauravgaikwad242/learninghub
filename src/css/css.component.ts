19ec05ef086324d5fa9db6936476938759412c26
gauravgaikwad242 <gaikwadg576@gmail.com>
2023-03-11 20:23:52 +0530

    added new module for css

---
+++ src/css/css.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'js-css',
+  templateUrl: './css.component.html',
+  styleUrls: ['./css.component.css']
+})
+export class CssComponent {
+
+}
