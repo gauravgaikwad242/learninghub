5e553a29b7a1718be97cc88a88921766d7962f3b
gauravgaikwad242 <gaikwadg576@gmail.com>
2023-03-11 20:22:41 +0530

    added basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IntroductionComponent } from './components/introduction/introduction.component';
+import { JavascriptComponent } from './components/javascript/javascript.component';
+import { CodeEditoryComponent } from './components/util/code-editory/code-editory.component';
 import { NotFoundComponent } from './components/util/not-found/not-found.component';
+import { TextEditorComponent } from './components/util/text-editor/text-editor.component';
 
 const routes: Routes = [
-  {path:"introduction", component:IntroductionComponent},
+  {path:'',component:JavascriptComponent},
+  {path:'javascript',component:JavascriptComponent,children:[
+    {path:"", component:IntroductionComponent},
+    {path:"introduction", component:IntroductionComponent},
+    {path:"editor",component:CodeEditoryComponent},
+    {path:"textEditor", component:TextEditorComponent},
+    {path:"**", component:NotFoundComponent},
+  ]},
+  { path: 'css', loadChildren: () => import('../css/css.module').then(m => m.CssModule) },
   {path:"**", component:NotFoundComponent},
 
 ];
--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { SideMenu } from 'src/app/model/side-menu';
+import { Order } from 'src/app/enum/order';
 @Component({
   selector: 'js-main',
   templateUrl: './main.component.html',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   opendrawer=true;
+  readonly Order=Order
+  sideMenu:SideMenu[]=[
+    new SideMenu(1,"/javascript/introduction","Introduction"),
+    new SideMenu(2,"/javascript/introduction","Introduction2"),
+    new SideMenu(3,"/javascript/introduction","Introduction3")
+  ]
+
+  // public get Order(){
+  //   return Order;
+  // }
 }
